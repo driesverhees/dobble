6a9c8f53b9a347bf8efb9475a533b78707615ff2
Dries Verhees <driesverhees@Driess-MBP.lan>
2018-01-14 20:07:45 +0100

    Improved card generator

--- main.jl
+++ main.jl
@@ -3,26 +3,37 @@ include("inverseCantorPairingRecursive.jl");
 include("binaryRepresentation.jl");
 include("isValidSet.jl");
 
-q = 4; # Number of symbols on each card, currently does not work for q >= 5 because an overflow for max will happen
+q = 5; # Number of symbols on each card
 m = q*(q-1)+1; # Number of symbols/card in total
 # Card representation: (i1,i2,..,iq) with i = 1..m
-#
-max = cantorPairingRecursive(m*ones(Int64,q));
-maxRepresentation = 2^(m+1)-1
-# The next variables are used to construct the set
+# The next variables are used to store the set
 setLength = 0;
 set = Array{Int64}(m);
-#
-for i = 1:max
-    cardSymbols = inverseCantorPairingRecursive(i,q);
+# Construct initial card (with symbols 1, 2, 3, ..., q)
+cardSymbols = Array{Int64}(1:q);
+set[1] = binaryRepresentation(cardSymbols, m);
+setLength = 1;
+print(string("Initial card: ",cardSymbols,"\n"));
+# Loop and generate cards
+while true
+# Construct the next card
+    for c=q:-1:1
+        if (cardSymbols[c] < m)
+            # The symbol can be increased
+            cardSymbols[c] = cardSymbols[c] + 1;
+            # The card symbols should be in increasing order, therefore update the remaing digits
+            if (c < q)
+                cardSymbols[c+1:q] = cardSymbols[c] + Array{Int64}(1:(q-c))
+            end
+            # Exit card generator
+            break;
+        end
+    end
+
+    # Calculate int representation, if any
 	card = binaryRepresentation(cardSymbols, m);
-	if (0 < card <= maxRepresentation)
-        #print(string(cardSymbols,"->",card,"->",bin(card,2),"\n"));
-        if (setLength == 0)
-            set[1] = card;
-            setLength = 1;
-            print(string("Card ", cardSymbols ," added","\n"));
-        elseif (!in(card, set))
+	if (0 < card)
+        if (!in(card, set))
             tmpSet = push!(set[1:setLength], card);
             validTmpSet = isValidSet(tmpSet);
             if (validTmpSet)
@@ -35,5 +46,9 @@ for i = 1:max
             end
         end
 	end
+    #
+    if (cardSymbols[1] >= m)
+        break;
+    end
 end
-print(string("-> Set contains ", setLength, " cards!","\n"))
+print(string("-> Set contains ", setLength, " cards! (Maximum: ",m,")\n"))
